4dfbb5d7aa2f2bdfeaf0226020857f0ae29606c5
chenjr16 <chenjr16@gmail.com>
2021-02-12 16:48:30 -0800

    Added some comments

--- src/com/casino/games/machines/slot/SlotMachine.java
+++ src/com/casino/games/machines/slot/SlotMachine.java
@@ -4,6 +4,8 @@ import com.casino.games.GameInterface;
 import com.casino.player.Dealer;
 import com.casino.player.Player;
 
+//author Junru Chen
+
 class SlotMachine implements GameInterface {
     @Override
     public boolean isPlayable(Player player, double bet) {
