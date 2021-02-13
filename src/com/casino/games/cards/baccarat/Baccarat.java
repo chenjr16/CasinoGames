daabebf42d25b36e6e52b6c69a91608a899023db
mrwindowsSDE <f3153808@avmtk.com>
2021-02-12 19:55:36 -0600

    change CasinoGames to public and added Pipe util class

--- src/com/casino/games/cards/baccarat/Baccarat.java
+++ src/com/casino/games/cards/baccarat/Baccarat.java
@@ -1,10 +1,15 @@
 package com.casino.games.cards.baccarat;
 
-import com.casino.games.GameInterface;
+import com.casino.games.CasinoGames;
+import static com.casino.games.cards.baccarat.utils.Pipe.apply;
 import com.casino.player.Dealer;
 import com.casino.player.Player;
 
-class Baccarat implements GameInterface {
+class Baccarat extends CasinoGames {
+    Player player;
+    Dealer dealer;
+    double bet;
+
     @Override
     public boolean isPlayable(Player player, double bet) {
         return false;
