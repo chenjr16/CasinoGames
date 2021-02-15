29e914da3ab8481f99d3414645c2cd05dda4fb69
BobaMilkTeea <marcobragado.edu@gmail>
2021-02-15 09:24:56 -0800

    Bets and Table class implementation added.

--- src/com/casino/games/board/roulette/Bets.java
+++ src/com/casino/games/board/roulette/Bets.java
@@ -1,4 +1,90 @@
 package com.casino.games.board.roulette;
 
+import java.util.Scanner;
+
+import static com.casino.games.board.roulette.Table.roulette;
+
 class Bets {
+    Scanner scan = new Scanner(System.in);
+    public void displayBets(){
+        Roulette roulette = new Roulette();
+        System.out.println("\nHere are the Bets you can make and their payouts:");
+        System.out.println(" ");
+        System.out.println("\n#\t\tBET TYPE\t\tPAYOUT");
+        System.out.println("----------------------------");
+        System.out.println("1)\t\tStraight\t\t35:1");
+        System.out.println("2)\t\tOdds/Evens\t\t1:1");
+        System.out.println("3)\t\tRed/Blacks\t\t1:1");
+        System.out.println("4)\t\tLows/Highs\t\t1:1");
+        System.out.println("5)\t\tDozens\t\t\t2:1");
+        System.out.println("6)\t\tColumns\t\t\t2:1");
+        System.out.println("7)\t\tStreet\t\t\t11:1");
+        System.out.println("8)\t\t6 Numbers\t\t5:1");
+        System.out.println("9)\t\tSplit\t\t\t17:1");
+        System.out.print("10)\t\tCorner\t\t\t8:1");
+        System.out.println("\n");
+        System.out.println("Please select a type of bet you would like to place [1 - 10]");
+    }
+
+    public int selectBetType(){
+        int betSelected;
+        do
+        {
+            System.out.printf("\nWhat kind of bet would you like to make [1-10]: ");
+
+            try
+            {
+                betSelected = Integer.valueOf(scan.nextLine());
+            }
+            catch (Exception e)
+            {
+                betSelected = 0;
+            }
+
+            if (betSelected > 0 && betSelected < 11)
+            {
+                return betSelected;
+            }
+            else
+            {
+                roulette.invalid();
+                System.out.print("Please select from the list: [1 - 10]\n");
+                betSelected = 0;
+            }
+        } while (betSelected == 0);
+        return betSelected;
+    }
+
+    public void playBetType(int betType) {
+        switch (betType) {
+            case 2:
+                roulette.oddsOrEvens();
+
+                break;
+            case 3:
+                roulette.redOrBlack();
+                break;
+            case 4:
+                roulette.lowOrHigh();
+                break;
+            case 5:
+                roulette.dozens();
+                break;
+            case 6:
+                roulette.columns();
+                break;
+            case 7:
+                roulette.street();
+                break;
+            case 8:
+                roulette.sixNumbers();
+                break;
+            case 9:
+                roulette.split();
+                break;
+            case 10:
+                roulette.corner();
+                break;
+        }
+    }
 }
--- src/com/casino/games/board/roulette/Table.java
+++ src/com/casino/games/board/roulette/Table.java
@@ -1,4 +1,58 @@
 package com.casino.games.board.roulette;
 
 class Table {
+    static Roulette roulette = new Roulette();
+    // SOURCE: https://www.youtube.com/watch?v=w-9ZTeO7q_E || 2D Array with Mike Dane
+    public static int[][] rouletteTableLayout = {{1,2,3}, {4,5,6}, {7,8,9}, {10,11,12}, {13,14,15}, {16,17,18}, {19,20,21}, {22,23,24}, {25,26,27}, {28,29,30}, {31,32,33}, {34,35,36} };
+    public int[] redNumbers = { 1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36 };
+    public int[] blackNumbers = { 2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35 };
+
+
+    public void displayRouletteTable(){
+        System.out.print("\t");
+        System.out.print("0");
+        System.out.print(" ");
+        System.out.println("00");
+        for (int i = 0; i < rouletteTableLayout.length; i++)
+        {
+            for (int j = 0; j < rouletteTableLayout[i].length; j++)
+            {
+                System.out.print("" + rouletteTableLayout[i][j] + "\t");
+                System.out.print(" ");
+            }
+            System.out.println(" ");
+        }
+    }
+
+
+    // Rolls the ball and will give the user the winning number
+    public void spinWheel() {
+        System.out.println("The wheel is spinning and the ball is rolling!! ");
+        for (int i = 0; i < 5; i ++){
+            try {
+                Thread.sleep(1000);
+                System.out.printf("o ");
+                System.out.printf("O ");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+        System.out.println("No more bets!");
+        for(int i = 0 ; i <= 2; i ++){
+            try {
+                Thread.sleep(500);
+                System.out.println("  o");
+                Thread.sleep(500);
+                System.out.println("    o");
+                Thread.sleep(500);
+                System.out.println("  o");
+                Thread.sleep(500);
+                System.out.println("    o");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("It looks like the ball has landed on .... " + roulette.getWinningNumber() );
+    }
 }
