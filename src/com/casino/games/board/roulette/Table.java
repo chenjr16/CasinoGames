a6c0b97a7a08cfdd215b7f79a953a0054876bef8
BobaMilkTeea <marcobragado.edu@gmail>
2021-02-17 01:32:50 -0800

    Color fields added to Table class

--- src/com/casino/games/board/roulette/Table.java
+++ src/com/casino/games/board/roulette/Table.java
@@ -2,6 +2,15 @@ package com.casino.games.board.roulette;
 
 class Table {
     static Roulette roulette = new Roulette();
+    // COLORS
+    // SOURCE : https://www.quora.com/Java-How-do-I-use-System-out-print-to-print-my-text-in-color-to-the-console
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
+    public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+    public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+
     // SOURCE: https://www.youtube.com/watch?v=w-9ZTeO7q_E || 2D Array with Mike Dane
     public static int[][] rouletteTableLayout = {{1,2,3}, {4,5,6}, {7,8,9}, {10,11,12}, {13,14,15}, {16,17,18}, {19,20,21}, {22,23,24}, {25,26,27}, {28,29,30}, {31,32,33}, {34,35,36} };
     public int[] redNumbers = { 1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36 };
